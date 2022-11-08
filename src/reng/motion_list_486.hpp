59a6fea62158f4f0dc0d2130cbe22f2d8ba15dcf
alphaZomega <alphazolam@users.noreply.github.com>
2022-11-07 22:55:23 -0500

    Add files via upload

--- src/reng/motion_list_486.hpp
+++ src/reng/motion_list_486.hpp
@@ -15,9 +15,124 @@
     along with this program.If not, see <https://www.gnu.org/licenses/>.
 */
 
+//This file updated by alphaZomega in 2022 for support of motlist.528, motlist.524, and motlist.484
+
 #pragma once
 #include "motion_458.hpp"
 #include "motion_list_85.hpp"
+
+class REMotlist653 : public REAssetBase {
+public:
+	uint64 pad;
+	esPointerX64<esPointerX64<REMotion495>> motions;
+	esPointerX64<char> unkOffset00;
+	esPointerX64<char16_t> fileName;
+	esPointerX64<char> null;
+	uint32 numMotions;
+	void Fixup();
+};
+
+typedef uni::VectorList<uni::Motion, REMotion495Asset> MotionList653;
+
+class REMotlist653Asset : public REAssetImpl,
+	public MotionList653,
+	public SkeletonList {
+	REMotlist653 &Get() { return REAssetBase::Get<REMotlist653>(this->buffer); }
+	const REMotlist653 &Get() const {
+		return REAssetBase::Get<const REMotlist653>(this->buffer);
+	}
+
+	uni::BaseElementConst AsSkeletons() const override {
+		return { static_cast<const SkeletonList *>(this), false };
+	}
+
+	uni::BaseElementConst AsMotions() const override {
+		return { static_cast<const MotionList653 *>(this), false };
+	}
+
+	void Fixup() override;
+	void Build() override;
+
+public:
+	static constexpr uint64 ID = CompileFourCC("mlst");
+	static constexpr uint64 VERSION = 653;
+};
+
+class REMotlist528 : public REAssetBase {
+public:
+	uint64 pad;
+	esPointerX64<esPointerX64<REMotion495>> motions;
+	esPointerX64<char> unkOffset00;
+	esPointerX64<char16_t> fileName;
+	esPointerX64<char> null;
+	uint32 numMotions;
+	void Fixup();
+};
+
+typedef uni::VectorList<uni::Motion, REMotion495Asset> MotionList528;
+
+class REMotlist528Asset : public REAssetImpl,
+	public MotionList528,
+	public SkeletonList {
+	REMotlist528 &Get() { return REAssetBase::Get<REMotlist528>(this->buffer); }
+	const REMotlist528 &Get() const {
+		return REAssetBase::Get<const REMotlist528>(this->buffer);
+	}
+
+	uni::BaseElementConst AsSkeletons() const override {
+		return { static_cast<const SkeletonList *>(this), false };
+	}
+
+	uni::BaseElementConst AsMotions() const override {
+		return { static_cast<const MotionList528 *>(this), false };
+	}
+
+	void Fixup() override;
+	void Build() override;
+
+public:
+	static constexpr uint64 ID = CompileFourCC("mlst");
+	static constexpr uint64 VERSION = 528;
+};
+
+
+class REMotlist524 : public REAssetBase {
+public:
+	uint64 pad;
+	esPointerX64<esPointerX64<REMotion492>> motions;
+	esPointerX64<char> unkOffset00;
+	esPointerX64<char16_t> fileName;
+	esPointerX64<char> null;
+	uint32 numMotions;
+	void Fixup();
+};
+
+typedef uni::VectorList<uni::Motion, REMotion492Asset> MotionList524;
+
+class REMotlist524Asset : public REAssetImpl,
+	public MotionList524,
+	public SkeletonList {
+	REMotlist524 &Get() { return REAssetBase::Get<REMotlist524>(this->buffer); }
+	const REMotlist524 &Get() const {
+		return REAssetBase::Get<const REMotlist524>(this->buffer);
+	}
+
+	uni::BaseElementConst AsSkeletons() const override {
+		return { static_cast<const SkeletonList *>(this), false };
+	}
+
+	uni::BaseElementConst AsMotions() const override {
+		return { static_cast<const MotionList524 *>(this), false };
+	}
+
+	void Fixup() override;
+	void Build() override;
+
+public:
+	static constexpr uint64 ID = CompileFourCC("mlst");
+	static constexpr uint64 VERSION = 524;
+};
+
 
 class REMotlist486 : public REAssetBase {
 public:
@@ -55,3 +170,42 @@ public:
   static constexpr uint64 ID = CompileFourCC("mlst");
   static constexpr uint64 VERSION = 486;
 };
+
+
+class REMotlist484 : public REAssetBase {
+public:
+	uint64 pad;
+	esPointerX64<esPointerX64<REMotion456>> motions;
+	esPointerX64<char> unkOffset00;
+	esPointerX64<char16_t> fileName;
+	esPointerX64<char> null;
+	uint32 numMotions;
+	void Fixup();
+};
+
+typedef uni::VectorList<uni::Motion, REMotion456Asset> MotionList484;
+
+class REMotlist484Asset : public REAssetImpl,
+	public MotionList484,
+	public SkeletonList {
+	REMotlist484 &Get() { return REAssetBase::Get<REMotlist484>(this->buffer); }
+	const REMotlist484 &Get() const {
+		return REAssetBase::Get<const REMotlist484>(this->buffer);
+	}
+
+	uni::BaseElementConst AsSkeletons() const override {
+		return { static_cast<const SkeletonList *>(this), false };
+	}
+
+	uni::BaseElementConst AsMotions() const override {
+		return { static_cast<const MotionList484 *>(this), false };
+	}
+
+	void Fixup() override;
+	void Build() override;
+
+public:
+	static constexpr uint64 ID = CompileFourCC("mlst");
+	static constexpr uint64 VERSION = 484;
+};
+
